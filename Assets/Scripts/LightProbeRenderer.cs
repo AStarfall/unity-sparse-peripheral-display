7956970fe1e884ddce282c2511130e9bcf9b601f
Marius Röhm <marius.roehm@gmail.com>
2023-05-29 18:51:29 +0200

    LightProbes Prefab + Renderer for Prefab

---
+++ Assets/Scripts/LightProbeRenderer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightProbeRenderer : MonoBehaviour
+{
+    public LightProbe[] lightProbes; // Array mit allen Light Probes
+
+    private RenderTexture renderTexture; // Render Texture für das Rendern der Light Probes
+
+    private void Start()
+    {
+
+    }
+}
+
