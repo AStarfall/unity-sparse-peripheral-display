180c986d094e7c6e180e7ccd0e5d4e413f3b2a21
Marius Röhm <marius.roehm@gmail.com>
2023-05-31 19:36:09 +0200

    performance improvements

--- Assets/Scripts/LightProbeRenderer.cs
+++ Assets/Scripts/LightProbeRenderer.cs
@@ -6,17 +6,32 @@ public class LightProbeRenderer : MonoBehaviour
 {
     public LightProbe[] lightProbes; // Array der Light Probes
 
-    public Camera mainCamera; // Referenz auf die MainCamera
+    public Camera renderCamera; // Referenz auf die MainCamera
     private Texture2D cameraTexture; // Textur, um das gerenderte Bild der MainCamera zu speichern
     private int[] nearestProbeIndices; // Zuordnung der Pixel zu den Light Probes
 
     void Start()
     {
+        // Skaliere die RenderTexture der MainCamera auf 1/4 der Größe
+        renderCamera.targetTexture = new RenderTexture(renderCamera.pixelWidth / 4, renderCamera.pixelHeight / 4, 24);
+
         // Erstelle die Textur, um das gerenderte Bild der MainCamera zu speichern
-        cameraTexture = new Texture2D(mainCamera.pixelWidth, mainCamera.pixelHeight);
+        cameraTexture = new Texture2D(renderCamera.pixelWidth, renderCamera.pixelHeight);
 
         // Berechne das Voronoi-Diagramm einmalig
         CalculateVoronoiDiagram();
+    }
+
+    void LateUpdate()
+    {
+        // Aktualisiere die Light Probes basierend auf dem vorberechneten Voronoi-Diagramm
+        StartCoroutine(UpdateLightProbes());
+
+        // // alle 10 Frames aktualisieren
+        // if (Time.frameCount % 10 == 0)
+        // {
+        //     StartCoroutine(UpdateLightProbes());
+        // }
     }
 
     void CalculateVoronoiDiagram()
@@ -57,19 +72,13 @@ public class LightProbeRenderer : MonoBehaviour
         }
     }
 
-    void LateUpdate()
-    {
-        // Aktualisiere die Light Probes basierend auf dem vorberechneten Voronoi-Diagramm
-        StartCoroutine(UpdateLightProbes());
-    }
-
     IEnumerator UpdateLightProbes()
     {
         yield return new WaitForEndOfFrame();
 
         // Rendere das Bild der MainCamera in die Textur
         RenderTexture currentRT = RenderTexture.active;
-        RenderTexture.active = mainCamera.targetTexture;
+        RenderTexture.active = renderCamera.targetTexture;
         cameraTexture.ReadPixels(new Rect(0, 0, cameraTexture.width, cameraTexture.height), 0, 0);
         cameraTexture.Apply();
         RenderTexture.active = currentRT;
@@ -81,6 +90,27 @@ public class LightProbeRenderer : MonoBehaviour
         // Erhalte die Pixelwerte der Kameratextur
         Color[] pixels = cameraTexture.GetPixels();
 
+        // ---- Berchnung der Durchschnittsfarbwerte in Batches ----
+        // int batchSize = 200;
+        // int numBatches = Mathf.CeilToInt((float)pixels.Length / batchSize);
+
+        // for (int batchIndex = 0; batchIndex < numBatches; batchIndex++)
+        // {
+        //     int startIndex = batchIndex * batchSize;
+        //     int endIndex = Mathf.Min(startIndex + batchSize, pixels.Length);
+
+        //     for (int i = startIndex; i < endIndex; i++)
+        //     {
+        //         int nearestProbeIndex = nearestProbeIndices[i];
+
+        //         averageColors[nearestProbeIndex] += pixels[i];
+        //         probeCounts[nearestProbeIndex]++;
+        //     }
+        // }
+        // ----
+
+
+        // ---- Berchnung der Durchschnittsfarbwerte ohne Batches ----
         // Iteriere über die Pixel und aktualisiere die Durchschnittsfarbwerte
         for (int i = 0; i < pixels.Length; i++)
         {
@@ -89,6 +119,9 @@ public class LightProbeRenderer : MonoBehaviour
             averageColors[nearestProbeIndex] += pixels[i];
             probeCounts[nearestProbeIndex]++;
         }
+        // ----
+
+        // Debug.Log("Width: " + cameraTexture.width + ", Height: " + cameraTexture.height + ", Total Pixels:" + pixels.Length);
 
         // Aktualisiere die Light Probes mit den Durchschnittsfarbwerten
         for (int i = 0; i < lightProbes.Length; i++)
@@ -109,7 +142,7 @@ public class LightProbeRenderer : MonoBehaviour
 
         for (int i = 0; i < lightProbes.Length; i++)
         {
-            float distance = Vector2.Distance(position, mainCamera.WorldToScreenPoint(lightProbes[i].GetProbePosition()));
+            float distance = Vector2.Distance(position, renderCamera.WorldToScreenPoint(lightProbes[i].GetProbePosition()));
             if (distance < nearestDistance)
             {
                 nearestIndex = i;
