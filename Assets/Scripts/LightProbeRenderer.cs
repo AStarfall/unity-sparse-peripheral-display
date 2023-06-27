88a540805b15fffc16d30c35fd9febdeb035a59d
Marius Röhm <marius.roehm@gmail.com>
2023-06-27 16:06:06 +0200

    test mit compute shader

--- Assets/Scripts/LightProbeRenderer.cs
+++ Assets/Scripts/LightProbeRenderer.cs
@@ -7,9 +7,12 @@ public class LightProbeRenderer : MonoBehaviour
     public GameObject lightProbeParent; // Referenz auf das GameObject, das alle Light Probes enthält
     private LightProbe[] lightProbes; // Array der Light Probes
 
+    // public ComputeShader computeShader; // Referenz auf den ComputeShader
+
     public Camera renderCamera; // Referenz auf die MainCamera
     private Texture2D cameraTexture; // Textur, um das gerenderte Bild der MainCamera zu speichern
     private int[] nearestProbeIndices; // Zuordnung der Pixel zu den Light Probes
+    int[] probeCounts; // Anzahl der Pixel, die einem Light Probe zugeordnet sind
 
     void Start()
     {
@@ -41,7 +44,7 @@ public class LightProbeRenderer : MonoBehaviour
         // Erstelle das Voronoi-Diagramm und berechne Durchschnittsfarbwerte
         nearestProbeIndices = new int[cameraTexture.width * cameraTexture.height];
         Color[] averageColors = new Color[lightProbes.Length];
-        int[] probeCounts = new int[lightProbes.Length];
+        probeCounts = new int[lightProbes.Length];
 
         for (int i = 0; i < cameraTexture.width; i++)
         {
@@ -74,6 +77,63 @@ public class LightProbeRenderer : MonoBehaviour
         }
     }
 
+    // IEnumerator UpdateLightProbes()
+    // {
+    //     yield return new WaitForEndOfFrame();
+
+    //     // Rendere das Bild der MainCamera in die Textur
+    //     RenderTexture currentRT = RenderTexture.active;
+    //     RenderTexture.active = renderCamera.targetTexture;
+    //     cameraTexture.ReadPixels(new Rect(0, 0, cameraTexture.width, cameraTexture.height), 0, 0);
+    //     cameraTexture.Apply();
+    //     RenderTexture.active = currentRT;
+
+    //     // Erstelle ComputeBuffers für nearestProbeIndices und probeCounts
+    //     ComputeBuffer nearestProbeIndicesBuffer = new ComputeBuffer(nearestProbeIndices.Length, sizeof(int));
+    //     ComputeBuffer probeCountsBuffer = new ComputeBuffer(this.probeCounts.Length, sizeof(int));
+
+    //     // Setze die Daten in die ComputeBuffers
+    //     nearestProbeIndicesBuffer.SetData(nearestProbeIndices);
+    //     probeCountsBuffer.SetData(probeCounts);
+
+    //     // Erhalte den Index des Kernels im ComputeShader
+    //     int kernelIndex = this.computeShader.FindKernel("CalculateAverageColors");
+
+    //     // Setze die ComputeBuffers im ComputeShader
+    //     computeShader.SetBuffer(kernelIndex, "nearestProbeIndices", nearestProbeIndicesBuffer);
+    //     computeShader.SetBuffer(kernelIndex, "probeCounts", probeCountsBuffer);
+
+    //     // Dispatch den ComputeShader
+    //     computeShader.Dispatch(kernelIndex, cameraTexture.width, cameraTexture.height, 1);
+
+    //     // Erhalte die aktualisierten Daten aus den ComputeBuffers
+    //     nearestProbeIndicesBuffer.GetData(nearestProbeIndices);
+    //     probeCountsBuffer.GetData(probeCounts);
+
+    //     // Gib die ComputeBuffers frei
+    //     nearestProbeIndicesBuffer.Release();
+    //     probeCountsBuffer.Release();
+
+    //     // Aktualisiere die Light Probes mit den Durchschnittsfarbwerten
+    //     Color[] averageColors = new Color[lightProbes.Length];
+
+    //     for (int i = 0; i < cameraTexture.width * cameraTexture.height; i++)
+    //     {
+    //         int nearestProbeIndex = nearestProbeIndices[i];
+
+    //         averageColors[nearestProbeIndex] += cameraTexture.GetPixel(i % cameraTexture.width, i / cameraTexture.width);
+    //     }
+
+    //     for (int i = 0; i < lightProbes.Length; i++)
+    //     {
+    //         if (probeCounts[i] > 0)
+    //         {
+    //             averageColors[i] /= probeCounts[i];
+    //             lightProbes[i].UpdateProbeColor(averageColors[i]);
+    //         }
+    //     }
+    // }
+
     IEnumerator UpdateLightProbes()
     {
         yield return new WaitForEndOfFrame();
@@ -87,7 +147,7 @@ public class LightProbeRenderer : MonoBehaviour
 
         // Weise den Light Probes die Farben basierend auf dem Voronoi-Diagramm zu
         Color[] averageColors = new Color[lightProbes.Length];
-        int[] probeCounts = new int[lightProbes.Length];
+        probeCounts = new int[lightProbes.Length];
 
         // Erhalte die Pixelwerte der Kameratextur
         Color[] pixels = cameraTexture.GetPixels();
